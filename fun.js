8859999379afc99390e6bd2571a4580eda7a5277
Mani Kumar <mani.malle.dev@gmail.com>
2026-04-03 22:29:14 +0530

    commit

---
+++ fun.js
@@ -0,0 +1,12 @@
+var username = "mani";
+var age = 20;
+var city = "chennai";
+console.log(
+  "My name is " +
+    username +
+    ", I am " +
+    age +
+    " years old, and I live in " +
+    city +
+    ".",
+);
